08789359e64fa69a92117d5a421cbb97b8952c08
Eugene <eugene.liujing.zh@gmail.com>
2018-07-19 23:06:54 +0800

    校对完成lecture07英文字幕

--- scripts/google_trans.py
+++ scripts/google_trans.py
@@ -15,23 +15,22 @@ def tran_sub(line):
     count += 1
     print("count,%d" % count)
     line = line.decode('utf-8')
-    if line.startswith('Dialogue') and count >= 324:
-        if ',English,,' in line:
-            des_file.write(line.encode('utf-8'))
-        if ',Chinese,,' in line:
-            head_str,english_str = tuple(line.rsplit(',,', 1))
-            chinese_str = translator.translate(english_str, dest='zh-CN').text
-            print(chinese_str)
-            des_file.write(',,'.join([head_str,chinese_str+'\n']).encode('utf-8'))
+    if line.startswith('Dialogue'):
+        des_file.write(line.encode('utf-8'))
+        head_str,english_str = tuple(line.rsplit(',,', 1))
+        chinese_str = translator.translate(english_str, dest='zh-CN').text
+        print(chinese_str)
+        c_line = ',,'.join([head_str,chinese_str+'\n']).replace('English', 'Chinese').encode('utf-8')
+        des_file.write(c_line)
     else:
         des_file.write(line.encode('utf-8'))
     gevent.sleep(1)
 
 def main():
-    sou_path = u""
-    des_path = u""
+    sou_path = u"/Users/eugene/workspace/translationCSAPP/subtitle/English/"
+    des_path = u"/Users/eugene/workspace/translationCSAPP/subtitle/Chinese_English/"
 
-    filename = u""
+    filename = u"Lecture 06  Machine Level Programming II  Control.ass"
 
     global sou_file
     global des_file
@@ -44,3 +43,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# for line in lines:
+#      if line.startswith('Dialogue:'):
+#         parts = line.rsplit(',,', 1)
+#         line = ',,'.join([parts[0], parts[1].lstrip().capitalize()])
+#     f.write(line)
